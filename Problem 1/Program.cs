1f78e488f8e250917ca453b2c47eb7d93c688bd0
Berik Bakhitov <bakhitov2001@mail.ru>
2019-02-16 15:40:53 +0600

    Lab 2

---
+++ Problem 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1 {
+    class Program {
+        static void Main(string[] args) {
+            string s;
+            StreamReader sr = new StreamReader(@"C:\Users\Lenovo\source\repos\Lab2\Problem 1\GG.txt");{
+             s = sr.ReadLine();
+                bool polindrome = true;
+                for(int i = 0; i < s.Length / 2; i++) {
+                    if(s[i] != s[s.Length - 1 - i]) {
+                        polindrome = false;
+                        break;
+                    }
+                } 
+             if(polindrome == true) {
+                    Console.WriteLine("Yes");
+                } else {
+                    Console.WriteLine("NO");
+                }
+
+            }
+            
+
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2 {
+    class Program {
+        static void Main(string[] args) {
+            using(StreamReader sr = new StreamReader(@"C:\Users\Lenovo\source\repos\Lab2\Problem2\input.txt")) {
+                string stroka = sr.ReadLine();
+                int[] m = Array.ConvertAll<string, int>(stroka.Split(), int.Parse);
+                int[] Prime = new int[m.Length];
+                int cnt = 0;
+                
+                for(int i = 0; i < m.Length; i++) {
+                    bool flag = true;
+                    for(int j = 2; j < m[i]; j++) {
+                        if( m[i] % j == 0) {
+                            flag = false;
+                            break;
+                        }
+
+                    }
+                    if(flag == true && m[i] != 1) {
+                        Prime[cnt] = m[i];
+                        cnt++;
+                    }
+                }
+                using(StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo\source\repos\Lab2\Problem2\output.txt")) {
+                    for(int i = 0; i < cnt; i++) {
+                        sw.Write(Prime[i] + " ");
+                    }
+
+                }
+            }  
+        }
+    }
+}
