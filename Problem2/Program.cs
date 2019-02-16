58a9da4f3ac4ec801f2f659044837d6601bac428
Berik Bakhitov <bakhitov2001@mail.ru>
2019-02-16 15:58:31 +0600

    Lab 2

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -8,27 +8,27 @@ using System.Threading.Tasks;
 namespace Problem2 {
     class Program {
         static void Main(string[] args) {
-            using(StreamReader sr = new StreamReader(@"C:\Users\Lenovo\source\repos\Lab2\Problem2\input.txt")) {
-                string stroka = sr.ReadLine();
-                int[] m = Array.ConvertAll<string, int>(stroka.Split(), int.Parse);
-                int[] Prime = new int[m.Length];
-                int cnt = 0;
+            using(StreamReader sr = new StreamReader(@"C:\Users\Lenovo\source\repos\Lab2\Problem2\input.txt")) {               // Открыл поток для чтения
+                string stroka = sr.ReadLine();                                                                                // Записал содержимое в строку
+                int[] m = Array.ConvertAll<string, int>(stroka.Split(), int.Parse);                                          // Создал массив
+                int[] Prime = new int[m.Length];                                                                            // Создал массив для простых чисел
+                int cnt = 0;                                                                                               // Создал счетчик для простых чисел
                 
-                for(int i = 0; i < m.Length; i++) {
-                    bool flag = true;
-                    for(int j = 2; j < m[i]; j++) {
+                for(int i = 0; i < m.Length; i++) {                                                                      // Пробегаюсь по всем числам в массиве 
+                    bool flag = true;                                                                                   // Создал флаг, отвечающий за добавление в Prime
+                    for(int j = 2; j < m[i]; j++) {                                                                    // Проверка числа на простое или сложное
                         if( m[i] % j == 0) {
                             flag = false;
                             break;
                         }
 
                     }
-                    if(flag == true && m[i] != 1) {
+                    if(flag == true && m[i] != 1) {                                                             // Если флаг tf == true и не равняется одному, то оно добавляется Prime числа
                         Prime[cnt] = m[i];
                         cnt++;
                     }
                 }
-                using(StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo\source\repos\Lab2\Problem2\output.txt")) {
+                using(StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo\source\repos\Lab2\Problem2\output.txt")) {    // Открыл поток чтобы записать
                     for(int i = 0; i < cnt; i++) {
                         sw.Write(Prime[i] + " ");
                     }
---
+++ Problem4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4 {
+    class Program {
+        static void Main(string[] args) {
+            FileInfo File = new FileInfo(@"C:\Users\Lenovo\source\repos\Lab2\Problem4\BB.txt");
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Lenovo\source\repos\Lab2\Problem4\Papka");
+            if(!dir.Exists) {
+                dir.Create();
+                FileInfo NewFile = new FileInfo(dir.FullName + @"\BB2.txt");
+                File.CopyTo(NewFile.FullName);
+                File.Delete();
+            } else {
+                dir.Delete(true);
+            }
+        }
+    }
+}
